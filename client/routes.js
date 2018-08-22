9846b6c1820c418394cbf2a2d1d89d3238ee436e
Pawel Bus <pawelbus.dev@outlook.com>
2018-08-22 13:30:32 +0200

    Put Kanban component into App in main routes.js

--- client/routes.js
+++ client/routes.js
@@ -16,27 +16,19 @@ if (typeof require.ensure !== 'function') {
  */
 if (process.env.NODE_ENV !== 'production') {
   // Require async routes only in development for react-hot-reloader to work.
-
+  require('./modules/Kanban/Kanban');
 }
 
 // react-router setup with code-splitting
 // More info: http://blog.mxstbr.com/2016/01/react-apps-with-pages/
 export default (
   <Route path="/" component={App}>
-    {/* <IndexRoute
+    <IndexRoute
       getComponent={(nextState, cb) => {
         require.ensure([], require => {
-          cb(null, require('./modules/Post/pages/PostListPage/PostListPage').default);
+          cb(null, require('./modules/Kanban/Kanban').default);
         });
       }}
     />
-    <Route
-      path="/posts/:slug-:cuid"
-      getComponent={(nextState, cb) => {
-        require.ensure([], require => {
-          cb(null, require('./modules/Post/pages/PostDetailPage/PostDetailPage').default);
-        });
-      }}
-    /> */}
   </Route>
 );
